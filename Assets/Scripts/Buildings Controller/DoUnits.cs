22d7dda7f933e37653ef989587f7567d5c3514da
SskytT <regmail21@yandex.ru>
2022-12-01 13:13:55 +0300

    1

--- Assets/Scripts/Buildings Controller/DoUnits.cs
+++ Assets/Scripts/Buildings Controller/DoUnits.cs
@@ -1,49 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//TODO: Переделать таймер
+//TODO: Написать скрипт спауна юнитов(будет компонентом здания, скорее всего, значит должен реагировать на команды здания)
+
 //Скрипт создания юнитов от зданий
 public class DoUnits : MonoBehaviour
 {
-    public float realTimeSpawning;
+    public double realTimeSpawning;
+    public GameObject myUnit;
     public BaseStructClass myStruct;
-    public Queue<GameObject> queueUnits;
-    public GameObject unit;
     public void Start()
     {
         myStruct = gameObject.GetComponent<BaseStructClass>();
-        queueUnits = new Queue<GameObject>();
     }
-    public void AddUnit(GameObject unit)
+    public void SetUnit(GameObject unit, double timeSpawning)
     {
-        queueUnits.Enqueue(unit);
+        realTimeSpawning = timeSpawning;
+        myUnit = unit;
     }
-    public void CreateUnit()
+    public void SpawnUnit()
     {
-        if (queueUnits.Count != 0 && unit == null)
-        {
-            unit = queueUnits.Peek();
-            realTimeSpawning = unit.GetComponent<BaseUnitClass>().ProductionTime;
-            print(realTimeSpawning);
-        }
-        if (unit != null)
+        if (myUnit != null && realTimeSpawning >= 0)
         {
             realTimeSpawning -= 0.1f;
             if (realTimeSpawning <= 0)
-            {
-                Instantiate(unit, gameObject.transform.Find("SpawnUnits").position, transform.rotation);
-                unit = null;
-                print("Completed");
-                queueUnits.Dequeue();
-            }
+                Instantiate(myUnit, gameObject.transform.Find("SpawnUnits").position, transform.rotation);
         }
+        if (realTimeSpawning <= 0)
+            myUnit = null;
     }
-    public void Update()
+    void Update()
     {
-        /*
-        if(Input.GetKeyDown(KeyCode.W) && myStruct.Selection.isSelected)
-            AddUnit(Resources.Load<GameObject>("Prefabs/Cyborg"));
-        */
-        CreateUnit();
+        if (myStruct.Selection.isSelected && Input.GetKeyDown("q"))
+            SetUnit(Resources.Load<GameObject>("Prefabs/Engineer"), 40);
+        if (myStruct.Selection.isSelected && Input.GetKeyDown("c"))
+            SetUnit(Resources.Load<GameObject>("Prefabs/Cyborg"), 40);
+        if (myStruct.Selection.isSelected && Input.GetKeyDown(KeyCode.Escape))
+            myUnit = null;
+            SpawnUnit();
     }
 }
