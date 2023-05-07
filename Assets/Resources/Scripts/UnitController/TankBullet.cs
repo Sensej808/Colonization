1baddb94e89efc9844e9bb43c1c9c98a91f01e7f
SskytT <regmail21@yandex.ru>
2023-05-07 19:54:04 +0300

    visual fix

--- Assets/Resources/Scripts/UnitController/TankBullet.cs
+++ Assets/Resources/Scripts/UnitController/TankBullet.cs
@@ -18,7 +18,6 @@ public class TankBullet : BaseBulletClass
     public void OnDestroy()
     {
         Collider2D[] hitColiders = Physics2D.OverlapCircleAll(gameObject.transform.position, 2);
-        int k = 0;
         foreach (Collider2D unit in hitColiders)
         {
             if (unit.gameObject != null && unit.gameObject.tag != gameObject.tag && unit.gameObject.GetComponent<Health>() != null)
